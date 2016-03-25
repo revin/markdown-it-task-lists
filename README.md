58776ee8e537112794cdd652dbf2b60f333dc411
Revin Guillen <rg@sevenite.com>
2016-03-25 11:05:05 -0700

    Add plugin, tests, license, readme

---
+++ README.md
@@ -0,0 +1,43 @@
+# markdown-it-task-lists
+
+A [markdown-it](https://www.npmjs.com/package/markdown-it) plugin to create GitHub-style [task lists](https://github.com/blog/1825-task-lists-in-all-markdown-documents)
+
+## What it does
+
+- Builds task/todo lists out of markdown lists with items starting with `[ ]` or `[x]`.
+- Nothing else
+
+### Why is this useful?
+
+When you have markdown documentation with checklists, rendering HTML checkboxes
+out of the list items looks nicer than the raw square brackets.
+
+## Installation
+
+```sh
+npm install markdown-it-task-lists
+```
+
+## Usage
+
+Use it the same as a normal markdown-it plugin:
+
+```js
+var md = require('markdown-it');
+var taskLists = require('markdown-it-task-lists');
+
+var parser = md().use(taskLists);
+
+var result = parser.render(...); // markdown string containing task list items
+```
+
+## Tests
+
+```sh
+npm install
+npm test
+```
+
+## License
+
+ISC
---
+++ test/index.js
@@ -0,0 +1,72 @@
+/* globals before, describe, it */
+
+var fs = require('fs');
+var assert = require('assert');
+var md = require('markdown-it');
+var cheerio = require('cheerio');
+var taskLists = require('..');
+
+describe('markdown-it-task-lists', function() {
+    var fixtures = {}, rendered = {}, $ = {}, parser;
+
+    before(function() {
+        var files = {
+            bullet: 'bullet.md',
+            ordered: 'ordered.md',
+            mixedNested: 'mixed-nested.md',
+            dirty: 'dirty.md'
+        };
+
+        parser = md().use(taskLists);
+
+        for (var key in files) {
+            fixtures[key] = fs.readFileSync(__dirname + '/fixtures/' + files[key]).toString();
+            rendered[key] = parser.render(fixtures[key]);
+            $[key] = cheerio.load(rendered[key]);
+        }
+    });
+
+    it('renders tab-indented code differently than default markdown-it', function() {
+        var parserDefault = md();
+        var parserWithPlugin = md().use(taskLists);
+        assert.notEqual(parserDefault.render(fixtures.bullet), parserWithPlugin.render(fixtures.bullet));
+    });
+
+    it('adds input.task-list-item-checkbox in items', function () {
+        assert(~$.bullet('input.task-list-item-checkbox').length);
+    });
+
+    it('renders items marked up as [ ] as unchecked', function () {
+        var shouldBeUnchecked = (fixtures.ordered.match(/[\.\*\+-]\s+\[ \]/g) || []).length;
+        assert.equal(shouldBeUnchecked, $.ordered('input[type=checkbox].task-list-item-checkbox:not(:checked)').length);
+    });
+
+    it('renders items marked up as [x] as checked', function () {
+        var shouldBeChecked = (fixtures.ordered.match(/[\.\*\+-]\s+\[x\]/g) || []).length;
+        assert.equal(shouldBeChecked, $.ordered('input[type=checkbox].task-list-item-checkbox:checked').length);
+    });
+
+    it('always disables the rendered checkboxes', function () {
+        assert(!$.bullet('input[type=checkbox].task-list-item-checkbox:not([disabled])').length);
+    });
+
+    it('does NOT render [  ], [ x], [x ], or [ x ] as checkboxes', function () {
+        var html = $.dirty.html();
+        assert(~html.indexOf('[  ]'));
+        assert(~html.indexOf('[x ]'));
+        assert(~html.indexOf('[ x]'));
+        assert(~html.indexOf('[ x ]'));
+    });
+
+    it('adds class .task-list-item to parent <li>', function () {
+        assert(~$.bullet('li.task-list-item').length);
+    });
+
+    it('adds class .task-list to lists', function () {
+        assert(~$.bullet('ol.task-list, ul.task-list').length);
+    });
+
+    it('only adds .task-list to most immediate parent list', function () {
+        assert($.mixedNested('ol:not(.task-list) ul.task-list').length);
+    });
+});
